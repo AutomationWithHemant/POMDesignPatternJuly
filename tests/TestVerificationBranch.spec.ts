a10355b875ab53f62699a6f2408e74531e51fe2c
Vikramdhar <vikycar4u2021@gmail.com>
2026-07-25 22:00:14 -0500

    updated review commits

--- tests/TestVerificationBranch.spec.ts
+++ tests/TestVerificationBranch.spec.ts
@@ -1,12 +1,8 @@
 import {test} from '../fixtures/common-fixtures'
 import CommonUtils from '../utils/commonUtils';
-test("Verification",async({page,loginPage,commonUtils})=>{
-    //const CommonUtilObj = new CommonUtils();
-    //CommonUtilObj.encryptData("standard_user")
-    // CommonUtilObj.encryptData("secret_sauce")
+test("Verification",async({page,loginPage,commonUtils})=>{  
     const decryptUserName = commonUtils.decryptData(process.env.USER_NAME!)
     const decryptPassword = commonUtils.decryptData(process.env.PASSWORD!)
-
     await loginPage.goToApplication(process.env.BASE_URL!)
     await loginPage.doLogin(decryptUserName,decryptPassword)
 })
